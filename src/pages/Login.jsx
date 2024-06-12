dc88c1d8469a0ac355aa15cfbf839aef6711d041
Anjali Sharma <sharmaanjali1241@gmail.com>
2024-06-12 23:36:32 +0530

    add more pages with basic info

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,18 @@
 function Login() {
   return (
-    <div>Login Page</div>
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">Chat App</span>
+        <span className="title">Register</span>
+        <form action="">
+            <input type="email" placeholder="email"></input>
+            <input type="password" placeholder="password"></input>
+           
+            <button>Sign In</button>
+            <p>Not have an account? Sign Up</p>
+        </form>
+      </div>
+    </div>
   )
 }
 
