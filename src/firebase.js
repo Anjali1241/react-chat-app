3b25531d92ef4927afa546a729d5c5d5dd764393
Anjali Sharma <sharmaanjali1241@gmail.com>
2024-06-17 22:28:46 +0530

    Add firebase configuration

---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB-SUteiDRrKvYfmm5LNOKMth915Eu5Je4",
+  authDomain: "chat-cd252.firebaseapp.com",
+  projectId: "chat-cd252",
+  storageBucket: "chat-cd252.appspot.com",
+  messagingSenderId: "194126522194",
+  appId: "1:194126522194:web:a7951165034a320b807c8b"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
+
+export const storage = getStorage();
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,19 +1,45 @@
+/* eslint-disable no-unused-vars */
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
+import { useState } from "react";
+
 function Register() {
+  const [err, setErr] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(e.target[0].value);
+    const displayName = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+    const file = e.target[3].files[0];
+    console.log(email, password);
+
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+    } catch (err) {
+      setErr(err);
+    }
+  };
+
   return (
     <div className="formContainer">
       <div className="formWrapper">
         <span className="logo">Chat App</span>
         <span className="title">Register</span>
-        <form action="">
-            <input type="text" placeholder="display name"></input>
-            <input type="email" placeholder="email"></input>
-            <input type="password" placeholder="password"></input>
-            <input type="file" id='file' className="fileSelection"/>
-            <label htmlFor="file"><img src="" alt=""  />
-            <span>Add an avatar</span></label>
-            <button>Sign Up</button>
-            <p>Have an account? Login</p>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="display name"></input>
+          <input type="email" placeholder="email"></input>
+          <input type="password" placeholder="password"></input>
+          <input type="file" id="file" className="fileSelection" />
+          <label htmlFor="file">
+            <img src="" alt="" />
+            <span>Add an avatar</span>
+          </label>
+          <button>Sign Up</button>
+          {err && <span>Something went wrong</span>}
         </form>
+        <p>Have an account? Login</p>
       </div>
     </div>
   );
